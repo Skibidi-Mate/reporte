700ce1adb56970343fc7fbd3342d17eccd366b14
Skibidi-Mate <mate.hernandezm@duocuc.cl>
2025-06-04 22:40:36 -0400

    se creo model reporte

--- src/main/java/com/api_reporte/ReporteApplication.java
+++ src/main/java/com/api_reporte/ReporteApplication.java
@@ -1,4 +1,4 @@
-package com.api_reporte.reporte;
+package com.api_reporte;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/api_reporte/models/Reporte.java
+++ src/main/java/com/api_reporte/models/Reporte.java
@@ -1,5 +1,29 @@
 package com.api_reporte.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "reportes")
+@Data
 public class Reporte {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idReporte;
+
+    @Column(nullable = false)
+    private String tipoReporte;
+
+    @Column(nullable = false)
+    private LocalDate fechaGeneracion;
+
+    @Column(length = 1000)
+    private String descripcion;
+
+    @Lob
+    @Column(name = "json_datos", columnDefinition = "TEXT")
+    private String jsonDatos;
 }
